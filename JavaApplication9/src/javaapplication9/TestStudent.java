75a4cf9ab02f19e5df175e7ecfffdec7ef90954b
Pinsajee Bungthong <35680302+Pinsajee@users.noreply.github.com>
2018-02-06 11:52:19 +0700

    Test Only

--- JavaApplication9/src/javaapplication9/TestStudent.java
+++ JavaApplication9/src/javaapplication9/TestStudent.java
@@ -3,12 +3,8 @@ public class TestStudent {
     public static void main(String[] args) {
         Address address = new Address("199/107" , "Bang Phil" , "Samutprakan");
         Faculty faculty = new Faculty("School of Information Technology" , "KMUTT");
-        ClassStudent student = new ClassStudent(60130500109L , "Pinsajee" , address , faculty);
-        address.setAddressname("199/107");
-        address.setCity("Bang Phil");
-        address.setProvince("Samutprakan");
-        faculty.setFacultyname("School of information Technology");
-        faculty.setUniversity("KMUTT");
+        ClassStudent student = new ClassStudent(60130500109L , "Pinsajee" , address("199/107" , "Banh Phil" , "Samutprakan") , faculty("SIT" , "KMUTT"));
+
         System.out.println("Address : " + address.getAddressname() + " " + address.getCity() + " " + address.getProvince());
         System.out.println("Faculty : " + faculty.getFacultyname() + " University : " + faculty.getUniversity());
     }
