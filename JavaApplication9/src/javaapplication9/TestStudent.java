f27019adfe91eba61c772ff302302d236a381599
Pinsajee Bungthong <35680302+Pinsajee@users.noreply.github.com>
2018-02-06 12:47:42 +0700

    Update TestStudent.java

--- JavaApplication9/src/javaapplication9/TestStudent.java
+++ JavaApplication9/src/javaapplication9/TestStudent.java
@@ -3,9 +3,13 @@ public class TestStudent {
     public static void main(String[] args) {
         Address address = new Address("199/107" , "Bang Phil" , "Samutprakan");
         Faculty faculty = new Faculty("School of Information Technology" , "KMUTT");
-        ClassStudent student = new ClassStudent(60130500109L , "Pinsajee" , address("199/107" , "Banh Phil" , "Samutprakan") , faculty("SIT" , "KMUTT"));
-
-        System.out.println("Address : " + address.getAddressname() + " " + address.getCity() + " " + address.getProvince());
+        Student student = new Student(60130500109L , "Pinsajee" , address , faculty);
+//        address.setAddress("199/107");
+//        address.setCity("Bang Phil");
+//        address.setProvince("Samutprakan");
+//        faculty.setFacultyname("School of Information Technology");
+//        faculty.setUniversity("KMUTT");
+        System.out.println("Address : " + address.getAddress()+ " " + address.getCity() + " " + address.getProvince());
         System.out.println("Faculty : " + faculty.getFacultyname() + " University : " + faculty.getUniversity());
     }
     
