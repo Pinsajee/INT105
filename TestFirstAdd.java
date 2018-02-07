3dfa910fbdaaff701db402586c31c2dca78011c5
Pinsajee Bungthong <35680302+Pinsajee@users.noreply.github.com>
2018-02-07 09:58:49 +0700

    Add files via upload

---
+++ TestFirstAdd.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author INT105
+ */
+class TestFirstAdd {
+
+    static int add2Integers(int x, int y) {
+        return -5;
+    }
+    
+}
