ea00636290cb0db8f1009037d587970a8108bab6
INT105 <INT105@10.4.22.132>
2018-02-19 11:51:08 +0700

    SelfBook

---
+++ JavaApplication21/src/javaapplication21/folder/Student.java
@@ -0,0 +1,30 @@
+package javaapplication21.folder;
+
+public class Student extends Person{
+    public static enum Status{freshman,sophomore, junior,senior};
+    Status status;
+
+    public Student(String name, Status status) {
+        super(name);
+        this.status = status;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nstatus=" + status;
+    }
+    
+    @Override
+    public String getSignature() {
+        return super.getName().substring(0, 2).toUpperCase()+"-"+super.getName().substring(super.getName().length()-2).toUpperCase();
+    } 
+    
+}
