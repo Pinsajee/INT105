8b2724c9c12fb3e2f18a7a1db57662a58f0611f4
INT105 <INT105@10.4.22.132>
2018-03-21 12:09:33 +0700

    Exception 2

--- Throwable/src/throwable/Throwable.java
+++ Throwable/src/throwable/Throwable.java
@@ -4,10 +4,13 @@ public class Throwable {
 
     public static void main(String[] args) {
         try {
-            System.out.println(5 / 0);
-        } catch (ArithmeticException object) {
+            System.out.println(5 / 1);
+            int[] number = {1, 2, 3, 4, 5};
+            System.out.println(number[5]);
+        } catch (RuntimeException object) {
             System.out.println("I can catch you");
+            System.out.println(object.getStackTrace());
+            object.printStackTrace();
         }
     }
-
 }
